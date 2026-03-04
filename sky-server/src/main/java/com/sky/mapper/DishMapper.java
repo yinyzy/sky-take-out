dd4ba4ad6fcd613e91b7efb055e3ae915b08e17a
yzy <3203086536@qq.com>
2026-03-04 21:48:18 +0800

    菜品分页查询

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,9 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.DishFlavor;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -25,4 +28,5 @@ public interface DishMapper {
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
 
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
